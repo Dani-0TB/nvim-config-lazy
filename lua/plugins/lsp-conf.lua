aeb28861115a0920a55d34de3905be2c852a32cb
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2025-03-11 21:00:46 -0300

    removed lsp-zero

--- lua/plugins/lsp-conf.lua
+++ lua/plugins/lsp-conf.lua
@@ -1,16 +1,5 @@
 return {
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    lazy = true,
-    config = false,
-    init = function()
-      -- Disable automatic setup, we are doing it manually
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
-  },
-  {
     'williamboman/mason.nvim',
     lazy = false,
     config = true,
@@ -24,27 +13,19 @@ return {
       {'L3MON4D3/LuaSnip'},
     },
     config = function()
-      -- Here is where you configure the autocompletion settings.
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_cmp()
-
       -- And you can configure cmp even more, if you want to.
       local cmp = require('cmp')
-      local cmp_action = lsp_zero.cmp_action()
 
       cmp.setup({
-        formatting = lsp_zero.cmp_format({details = true}),
         mapping = cmp.mapping.preset.insert({
-          ['<CR>'] = cmp.mapping.confirm({select= false}),
-          ['<Tab'] = cmp.mapping.complete(),
+          ['<CR>']  = cmp.mapping.confirm({select= false}),
+          ['<Tab']  = cmp.mapping.complete(),
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
         }),
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            vim.snippet.expand(args.body)
           end,
         },
       })
@@ -57,23 +38,44 @@ return {
     event = {'BufReadPre', 'BufNewFile'},
     dependencies = {
       {'hrsh7th/cmp-nvim-lsp'},
+      {'williamboman/mason.nvim'},
       {'williamboman/mason-lspconfig.nvim'},
     },
+    init = function()
+      -- reserve space in the gutter
+      vim.opt.signcolumn = "yes"
+    end,
     config = function()
-      -- This is where all the LSP shenanigans will live
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_lspconfig()
+      local lsp_defaults = require('lspconfig').util.default_config
 
-      --- if you want to know more about lsp-zero and mason.nvim
-      --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({buffer = bufnr})
-      end)
+      lsp_defaults.capabilities = vim.tbl_deep_extend(
+         'force',
+         lsp_defaults.capabilities,
+         require('cmp_nvim_lsp').default_capabilities()
+      )
+
+      -- LspAttach is where you enable features that only work
+      -- if there is a language server active in the file
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        callback = function(event)
+          local opts = {buffer = event.buf}
+
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+          vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+          vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+          vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+          vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        end,
+      })
 
       require('mason-lspconfig').setup({
-        ensure_installed = {"clangd","lua_ls", "tsserver", "rust_analyzer"},
+        ensure_installed = {"clangd","lua_ls", "ts_ls", "rust_analyzer", "lua_ls"},
         handlers = {
             -- this first function is the "default handler"
             -- it applies to every language server without a "custom handler"
@@ -81,24 +83,30 @@ return {
                 require('lspconfig')[server_name].setup({})
               end,
 
-              -- this is the "custom handler" for `lua_ls`
               lua_ls = function()
-                -- (Optional) Configure lua language server for neovim
-                local lua_opts = lsp_zero.nvim_lua_ls()
-                require('lspconfig').lua_ls.setup(lua_opts)
+                require('lspconfig').lua_ls.setup({
+                  settings = {
+                    Lua = {
+                      diagnostics = {
+                        globals = {"vim"}
+                      }
+                    }
+                  }
+                })
               end,
 
               volar = function()
                 require('lspconfig').volar.setup({})
               end,
-              tsserver = function()
+
+              ts_ls = function()
                   local vue_typescript_plugin = require('mason-registry')
                     .get_package('vue-language-server')
                     :get_install_path()
                     .. '/node_modules/@vue/language-server'
                     .. '/node_modules/@vue/typescript-plugin'
 
-                  require('lspconfig').tsserver.setup({
+                  require('lspconfig').ts_ls.setup({
                     init_options = {
                       plugins = {
                         {

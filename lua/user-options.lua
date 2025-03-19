0c2c38d9f84313dc252fd3dd4d46e5266031f226
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2025-03-19 01:08:19 -0300

    removed unused plugins and added new bindings

--- lua/user-options.lua
+++ lua/user-options.lua
@@ -29,3 +29,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-d>", "25jzz")
 vim.keymap.set("n", "<C-u>", "25kzz")
+
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+

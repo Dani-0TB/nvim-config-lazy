9a057d9dfdd51cba48fd09988da222e4fd8dab2f
Dani Ocaranza <docaranzan@gmail.com>
2025-04-11 20:22:09 -0400

    added mini.move and mini.pairs removed autopairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,13 +1,19 @@
 return {
-    {
-        'echasnovski/mini.nvim',
-        version = false,
-        config = function ()
-            local icons = require 'mini.icons'
-            icons.setup {}
+  {
+    'echasnovski/mini.nvim',
+    version = false,
+    config = function ()
+      local icons = require 'mini.icons'
+      icons.setup {}
 
-            local surround = require 'mini.surround'
-            surround.setup {}
-        end
-    }
+      local surround = require 'mini.surround'
+      surround.setup {}
+
+      local pairs = require 'mini.pairs'
+      pairs.setup {}
+
+      local move = require 'mini.move'
+      move.setup {}
+    end
+  }
 }
